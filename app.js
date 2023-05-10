d8a1bd47f22318ebc492384fd99aa53ac5ba0bd5
WinstonAdams <apollo07291@gmail.com>
2023-05-10 22:59:06 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
-const restaurantsList = require('./restaurant.json').results
+const bodyParser = require('body-parser')
+const Restaurant = require('./models/restaurant')
+const e = require('express')
 
 const app = express()
 
@@ -30,32 +32,60 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
 //- 根目錄，瀏覽全部所有餐廳
 app.get('/', (req, res) => {
-  // 傳入的 key 和 value 名稱一樣，可以簡寫
-  res.render('index', { restaurantsList })
+  Restaurant.find()
+    .lean()
+    .then(restaurantsList => res.render('index', { restaurantsList }))
+    .catch(error => console.log(error))
+})
+
+//- 進入新增餐廳頁面
+app.get('/restaurants/new', (req, res) => {
+  res.render('new')
+})
+
+//- 新增餐廳
+app.post('/restaurants', (req, res) => {
+  Restaurant.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 //- 瀏覽一家餐廳的詳細資訊
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant_id = req.params.restaurant_id
-  const restaurantFound = restaurantsList.find(restaurant => restaurant.id.toString() === restaurant_id)
-
-  res.render('show', { restaurant: restaurantFound })
+  Restaurant.findById(restaurant_id)
+    .lean()
+    .then(restaurant => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
 })
 
 //- 搜尋餐廳
 app.get('/search', (req, res) => {
+  const keywords = req.query.keyword
   const keyword = req.query.keyword.trim().toLowerCase()
+
   // 若 keyword 沒有輸入內容，重新導向 / 路由(根目錄)
   if (!keyword) {
     return res.redirect('/')
   }
-  const restaurantFiltered = restaurantsList.filter(restaurant => restaurant.name.toLowerCase().includes(keyword) ||
-    restaurant.category.toLowerCase().includes(keyword))
 
-  res.render('index', { restaurantsList: restaurantFiltered, keyword })
+  Restaurant.find()
+    .lean()
+    .then(restaurants => {
+      const restaurantFiltered = restaurants.filter(
+        restaurant =>
+          restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword))
+
+      res.render('index', { restaurantsList: restaurantFiltered, keywords })
+    })
+    .catch(error => console.log(error))
 })
+
 
 
 
