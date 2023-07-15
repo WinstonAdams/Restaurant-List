71b634041f3d38dbe2e9ece306690f459157ee07
WinstonAdams <apollo07291@gmail.com>
2023-07-15 22:31:54 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const flash = require('connect-flash')
 
 //* 載入自己設定的檔案
 // 載入路由器 (自動尋找資料夾中的 index.js)
@@ -43,9 +44,14 @@ app.use(methodOverride('_method'))
 
 usePassport(app)
 
+app.use(flash())
+
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.error_msg = req.flash('error_msg')
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -15,6 +15,7 @@ router.post('/login', passport.authenticate('local', {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
@@ -24,11 +25,25 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  // 若 errors 陣列中有元素時
+  if (errors.length) {
+    // errors 傳給 register，再傳給 message
+    return res.render('register', { errors, name, email, password, confirmPassword })
+  }
 
   User.findOne({ email })
     .then(user => {
       if (user) {
-        return res.render('register', { name, email, password, confirmPassword })
+        errors.push({ message: '這個 Email 已經註冊過了。' })
+        return res.render('register', { errors, name, email, password, confirmPassword })
       }
       return User.create({ name, email, password })
         .then(() => res.redirect('/'))
