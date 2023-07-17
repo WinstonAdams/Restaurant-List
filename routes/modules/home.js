ac2e85a1767a8d756ee1e748face2e137e4fb5ca
WinstonAdams <apollo07291@gmail.com>
2023-07-17 11:53:24 +0800

    refactor home route and modify .env.example

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -21,23 +21,24 @@ router.get('/search', (req, res) => {
   const UserId = req.user._id
   // sort 被賦值為 dropdown box 選擇到的元素的 value
   const { keyword, sort } = req.query
-  const keywordLowerCase = req.query.keyword.trim().toLowerCase()
 
   // 若 keyword 沒有輸入內容，重新導向 / 路由(根目錄)
   if (!keyword) {
     return res.redirect('/')
   }
 
-  Restaurant.find({ UserId })
+  Restaurant.find({
+    UserId,
+    $or: [  // $or：符合其中一個條件即可
+      { name: { $regex: keyword, $options: 'ix' } },  // i：不分大小寫； x：忽略所有空白字符
+      { category: { $regex: keyword, $options: 'ix' } }
+    ]
+  })
     .lean()
     .sort(sort)
     .then(restaurants => {
-      const restaurantFiltered = restaurants.filter(
-        restaurant =>
-          restaurant.name.toLowerCase().includes(keywordLowerCase) || restaurant.category.toLowerCase().includes(keywordLowerCase))
-
-      if (restaurantFiltered.length) {
-        res.render('index', { restaurantsList: restaurantFiltered, keyword, sort })
+      if (restaurants.length) {
+        res.render('index', { restaurantsList: restaurants, keyword, sort })
       } else {
         res.render('searchNoResult', { keyword, sort })
       }
